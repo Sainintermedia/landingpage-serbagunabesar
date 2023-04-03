a69e4374938cd9b40ac40777ebf3706fcac7cb96
hiarief <arhistr14@gmail.com>
2023-04-03 21:24:20 +0700

    */landing-page

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,69 +1,64 @@
 <header>
-    <div class="container">
-    {{-- <nav class="navbar navbar-light" style="background-color: #e3f2fd;"> --}}
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">CV. SERBAGUNA BERSAR</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="container"></div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">CV. SERBAGUNA BERSAR</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Tentang Kami</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#">Kontak</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Produk
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li><a class="dropdown-item" href="#">Drum Kaleng</a></li>
-                      <li><a class="dropdown-item" href="#">Drum Pelastik</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Palet Pelastik</a></li>
-                      <li><a class="dropdown-item" href="#">Karung</a></li>
-                      <li><a class="dropdown-item" href="#">Kardus</a></li>
-                      <li><a class="dropdown-item" href="#">Drigen</a></li>
-                      <li><a class="dropdown-item" href="#">Bag Container</a></li>
-                      <li><a class="dropdown-item" href="#">Kempu</a></li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Service Kami</a>
-                  </li>
-                  <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Download</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Login</a>
-                    @auth
-                    {{ auth()->user()->name }}
-                    <div class="text-end">
-                        <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2">Logout</a>
-                    </div>
-                    <div class="text-end">
-                        <a href="{{ route('dashboard.index') }}" class="btn btn-outline-light me-2">Dashboard</a>
-                    </div>
+            </button>
+            <div class="navbar-collapse collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-lg-0 mb-2">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Tentang Kami</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Kontak</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Produk
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Drum Kaleng</a></li>
+                            <li><a class="dropdown-item" href="#">Drum Pelastik</a></li>
+                            <li><a class="dropdown-item" href="#">Palet Pelastik</a></li>
+                            <li><a class="dropdown-item" href="#">Karung</a></li>
+                            <li><a class="dropdown-item" href="#">Kardus</a></li>
+                            <li><a class="dropdown-item" href="#">Drigen</a></li>
+                            <li><a class="dropdown-item" href="#">Bag Container</a></li>
+                            <li><a class="dropdown-item" href="#">Kempu</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Service
+                            Kami</a>
+                    </li>
+                    <li>
+                        <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Download</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        @auth
+                            {{ auth()->user()->name }}
+                        <li><a class="nav-link active" href="{{ route('logout.perform') }}" tabindex="-1"
+                                aria-disabled="true">Logout</a></li>
+                        <li><a class="nav-link active" href="{{ route('dashboard.index') }}" tabindex="-1"
+                                aria-disabled="true">Dashboard</a></li>
                     @endauth
+                    </li>
 
-                    @guest
-                        <div class="text-end">
-                            <a href="{{ route('login.perform') }}" class="btn btn-outline-success">Login</a>
-                            {{--  <a href="{{ route('register.perform') }}" class="btn btn-warning">Sign-up</a>  --}}
-                        </div>
-                    @endguest
-                </ul>
-                {{-- <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form> --}}
-                
-              </div>
+                    <li>
+                        @guest
+                            <a class="nav-link active" href="{{ route('login.perform') }}" tabindex="-1"
+                                aria-disabled="true">Login</a>
+                        @endguest
+                    </li>
+
             </div>
-        </nav>
-                
+        </div>
+    </nav>
+
     </div>
     {{-- <div class="container">
         <div class="d-flex align-items-center justify-content-center justify-content-lg-start flex-wrap">
@@ -86,7 +81,7 @@
                     aria-label="Search">
             </form>
 
-         
+
         </div>
     </div> --}}
 </header>
