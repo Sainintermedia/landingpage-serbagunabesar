14882fd8d4bb0595ce9c3dfbf3661dee673bd22a
hiarief <arhistr14@gmail.com>
2023-04-03 21:48:13 +0700

    */indexing-landing-page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,66 +1,70 @@
 @extends('layouts.app-master')
 
 @section('content')
-        <div class="bg-light rounded p-1">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
+    <div class="bg-light rounded p-1">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img src="{!! url('assets/image/slide_01.jpg') !!}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="{!! url('assets/image/slide_02.jpg') !!}"  class="d-block w-100" alt="...">
+                    <img src="{!! url('assets/image/slide_02.jpg') !!}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="{!! url('assets/image/slide_03.jpg')!!}" class="d-block w-100" alt="...">
+                    <img src="{!! url('assets/image/slide_03.jpg') !!}" class="d-block w-100" alt="...">
                 </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
-                </button>
-            </div>
+            </button>
         </div>
-        <div class="bg-light rounded p-3">
-            {{-- @auth
-                <h1>Dashboard</h1>
-                <p class="lead">Only authenticated users can access this section.</p>
-                <a class="btn btn-lg btn-primary" href="https://codeanddeploy.com" role="button">View more tutorials here &raquo;</a>
-            @endauth --}}
+    </div>
+    <div class="bg-light rounded p-3">
 
-            
-    
-                <div class="six_box">
-                    <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-2 col-sm-4 pa_left">
-                        <div class="six_probpx yellow_bg"><i><img src="{!! url('assets/image/drumpelastik.png') !!}" alt="website template image"></i> <span>Drum Pelastik</span></div>
-                        </div>
-                        <div class="col-md-2 col-sm-4 pa_left">
-                        <div class="six_probpx bluedark_bg"><i><img src="{!! url('assets/image/drumkaleng.png') !!}" alt="website template image"></i> <span>Drum Kaleng</span></div>
-                        </div>
-                        <div class="col-md-2 col-sm-4 pa_left">
-                        <div class="six_probpx yellow_bg"><i><img src="{!! url('assets/image/karung.png') !!}" alt="website template image"></i> <span>Karung</span></div>
-                        </div>
-                        <div class="col-md-2 col-sm-4 pa_left">
-                        <div class="six_probpx bluedark_bg"><i><img src="{!! url('assets/image/kardus.png') !!}" alt="website template image"></i> <span>kardus</span></div>
-                        </div>
-                        <div class="col-md-2 col-sm-4 pa_left">
-                        <div class="six_probpx yellow_bg"><i><img src="{!! url('assets/image/paletpelastik.png') !!}" alt="website template image"></i> <span>Palet Pelastik</span></div>
-                        </div>
-                        <div class="col-md-2 col-sm-4 pa_left">
-                        <div class="six_probpx bluedark_bg"><i><img src="{!! url('assets/image/kempu.png') !!}" alt="website template image"></i> <span>Kempu</span></div>
-                        </div>
+        <div class="six_box">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-2 col-sm-4 pa_left">
+                        <div class="six_probpx yellow_bg"><i><img src="{!! url('assets/image/drumpelastik.png') !!}"
+                                    alt="website template image"></i> <span>Drum Pelastik</span></div>
                     </div>
+                    <div class="col-md-2 col-sm-4 pa_left">
+                        <div class="six_probpx bluedark_bg"><i><img src="{!! url('assets/image/drumkaleng.png') !!}"
+                                    alt="website template image"></i> <span>Drum Kaleng</span></div>
+                    </div>
+                    <div class="col-md-2 col-sm-4 pa_left">
+                        <div class="six_probpx yellow_bg"><i><img src="{!! url('assets/image/karung.png') !!}"
+                                    alt="website template image"></i> <span>Karung</span></div>
+                    </div>
+                    <div class="col-md-2 col-sm-4 pa_left">
+                        <div class="six_probpx bluedark_bg"><i><img src="{!! url('assets/image/kardus.png') !!}"
+                                    alt="website template image"></i> <span>kardus</span></div>
+                    </div>
+                    <div class="col-md-2 col-sm-4 pa_left">
+                        <div class="six_probpx yellow_bg"><i><img src="{!! url('assets/image/paletpelastik.png') !!}"
+                                    alt="website template image"></i> <span>Palet Pelastik</span></div>
+                    </div>
+                    <div class="col-md-2 col-sm-4 pa_left">
+                        <div class="six_probpx bluedark_bg"><i><img src="{!! url('assets/image/kempu.png') !!}"
+                                    alt="website template image"></i> <span>Kempu</span></div>
                     </div>
                 </div>
+            </div>
         </div>
+    </div>
 @endsection
--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,66 +1,65 @@
-<header>
-    <div class="container"></div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">CV. SERBAGUNA BERSAR</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="navbar-collapse collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-lg-0 mb-2">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Tentang Kami</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Kontak</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Produk
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Drum Kaleng</a></li>
-                            <li><a class="dropdown-item" href="#">Drum Pelastik</a></li>
-                            <li><a class="dropdown-item" href="#">Palet Pelastik</a></li>
-                            <li><a class="dropdown-item" href="#">Karung</a></li>
-                            <li><a class="dropdown-item" href="#">Kardus</a></li>
-                            <li><a class="dropdown-item" href="#">Drigen</a></li>
-                            <li><a class="dropdown-item" href="#">Bag Container</a></li>
-                            <li><a class="dropdown-item" href="#">Kempu</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Service
-                            Kami</a>
-                    </li>
-                    <li>
-                        <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Download</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        @auth
-                            {{ auth()->user()->name }}
-                        <li><a class="nav-link active" href="{{ route('logout.perform') }}" tabindex="-1"
-                                aria-disabled="true">Logout</a></li>
-                        <li><a class="nav-link active" href="{{ route('dashboard.index') }}" tabindex="-1"
-                                aria-disabled="true">Dashboard</a></li>
-                    @endauth
-                    </li>
+<header></header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">CV. SERBAGUNA BESAR</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-lg-0 mb-2">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Tentang Kami</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Kontak</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Produk
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Drum Kaleng</a></li>
+                        <li><a class="dropdown-item" href="#">Drum Pelastik</a></li>
+                        <li><a class="dropdown-item" href="#">Palet Pelastik</a></li>
+                        <li><a class="dropdown-item" href="#">Karung</a></li>
+                        <li><a class="dropdown-item" href="#">Kardus</a></li>
+                        <li><a class="dropdown-item" href="#">Drigen</a></li>
+                        <li><a class="dropdown-item" href="#">Bag Container</a></li>
+                        <li><a class="dropdown-item" href="#">Kempu</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Service
+                        Kami</a>
+                </li>
+                <li>
+                    <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Download</a>
+                </li>
+                <li class="nav-item dropdown">
+                    @auth
+                        {{ auth()->user()->name }}
+                    <li><a class="nav-link active" href="{{ route('logout.perform') }}" tabindex="-1"
+                            aria-disabled="true">Logout</a></li>
+                    <li><a class="nav-link active" href="{{ route('dashboard.index') }}" tabindex="-1"
+                            aria-disabled="true">Dashboard</a></li>
+                @endauth
+                </li>
 
-                    <li>
-                        @guest
-                            <a class="nav-link active" href="{{ route('login.perform') }}" tabindex="-1"
-                                aria-disabled="true">Login</a>
-                        @endguest
-                    </li>
+                <li>
+                    @guest
+                        <a class="nav-link active" href="{{ route('login.perform') }}" tabindex="-1"
+                            aria-disabled="true">Login</a>
+                    @endguest
+                </li>
 
-            </div>
         </div>
-    </nav>
-
     </div>
-    {{-- <div class="container">
+</nav>
+
+</div>
+{{-- <div class="container">
         <div class="d-flex align-items-center justify-content-center justify-content-lg-start flex-wrap">
             <a href="/" class="d-flex align-items-center mb-lg-0 text-decoration-none mb-2 text-white">
                 <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
