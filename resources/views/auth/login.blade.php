84f590331c1606548ced5406c5d97002f87512cb
hiarief <arhistr14@gmail.com>
2023-04-04 08:07:14 +0700

    */login-setup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,77 +2,84 @@
 <html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ _('Login') }} | {{ _('Serbagunabisa') }}</title>
 
-    <title>Gentelella Alela! |</title>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-    <!-- Bootstrap -->
-    <link href="../assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <!-- Font Awesome -->
-    <link href="../assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-    <!-- NProgress -->
-    <link href="../assets/vendors/nprogress/nprogress.css" rel="stylesheet" />
-    <!-- Animate.css -->
-    <link href="../assets/vendors/animate.css/animate.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ URL::asset('assets/dashboard/plugins/fontawesome-free/css/all.min.css') }}">
 
-    <!-- Custom Theme Style -->
-    <link href="../assets/build/css/custom.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ URL::asset('assets/dashboard/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+
+    <link rel="stylesheet" href="{{ URL::asset('assets/dashboard/dist/css/adminlte.min.css') }}">
 </head>
 
-<body class="login">
-    <div>
-        <a class="hiddenanchor" id="signup"></a>
-        <a class="hiddenanchor" id="signin"></a>
+<body class="hold-transition login-page">
+    <div class="login-box">
 
-        <div class="login_wrapper">
-            <div class="animate form login_form">
-                <section class="login_content">
-
-                    @include('layouts.partials.messages')
-
-                    <form method="post" action="{{ route('login.perform') }}">
-                        @csrf
-                        <h1>Login</h1>
-                        <div>
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="{{ route('home.index') }}" class="h1"><b>Serbaguna</b>BISA</a>
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+                <form method="post" action="{{ route('login.perform') }}">
+                    @csrf
+                    <div>
+                        <div class="input-group mb-3">
                             <input type="text" class="form-control" name="username" value="{{ old('username') }}"
                                 placeholder="Email or Username" required="" autofocus />
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
                             @if ($errors->has('username'))
                                 <span class="text-danger text-left">{{ $errors->first('username') }}</span>
                             @endif
                         </div>
-                        <div>
+                    </div>
+                    <div>
+                        <div class="input-group mb-3">
                             <input type="password" class="form-control" name="password" value="{{ old('password') }}"
                                 placeholder="Password" required="" />
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
                             @if ($errors->has('password'))
                                 <span class="text-danger text-left">{{ $errors->first('password') }}</span>
                             @endif
                         </div>
-                        <div>
-                            <button class="btn btn-default submit" type="submit" href="index.html">Log in</button>
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <div class="separator">
-
-                            <div class="clearfix"></div>
-                            <br />
-
-                            <div>
-                                <h1>CV. Serbaguna Besar</h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
                             </div>
                         </div>
-                    </form>
-                </section>
-            </div>
 
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-    </div>
+
+    <script src="{{ URL::asset('assets/dashboard/plugins/jquery/jquery.min.js') }}"></script>
+
+    <script src="{{ URL::asset('assets/dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ URL::asset('assets/dashboard/dist/js/adminlte.min.js') }}"></script>
 </body>
 
 </html>
