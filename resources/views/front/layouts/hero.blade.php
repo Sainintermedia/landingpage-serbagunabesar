5c60a25780b489a874e9ba10749bc0afc35e3b15
hiarief <arhistr14@gmail.com>
2023-05-23 15:16:32 +0700

    */front-content

--- resources/views/front/layouts/hero.blade.php
+++ resources/views/front/layouts/hero.blade.php
@@ -1,63 +1,63 @@
 <section id="hero" class="hero">
-    <div class="position-relative container">
-        <div class="row gy-5" data-aos="fade-in">
-            <div class="col-lg-6 order-lg-1 d-flex flex-column justify-content-center text-lg-start order-2 text-center">
-                <h2>Selamat datang di <span>SerbagunaBisa</span></h2>
-                <p>Sebuah perusahaan pengelolaan limbah yang berdedikasi untuk mengurangi dampak negatif limbah terhadap
-                    lingkungan. Misi utama perusahaan ini adalah untuk menyediakan solusi terpadu dalam pengelolaan
-                    limbah agar mencapai keberlanjutan dan keberhasilan ekonomi.</p>
-                <div class="d-flex justify-content-center justify-content-lg-start">
-                    <a href="#services" class="btn-get-started">Mulai</a>
-                    {{--  <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+    <div class="position-relative container"></div>
+    <div class="row gy-5" data-aos="fade-in">
+        <div class="col-lg-6 order-lg-1 d-flex flex-column justify-content-center text-lg-start order-2 text-center">
+            <h2>Selamat datang di <span>SerbagunaBisa</span></h2>
+            <p>Sebuah perusahaan pengelolaan limbah yang berdedikasi untuk mengurangi dampak negatif limbah terhadap
+                lingkungan. Misi utama perusahaan ini adalah untuk menyediakan solusi terpadu dalam pengelolaan
+                limbah agar mencapai keberlanjutan dan keberhasilan ekonomi.</p>
+            <div class="d-flex justify-content-center justify-content-lg-start">
+                <a href="#services" class="btn-get-started">Mulai</a>
+                {{--  <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
                         class="glightbox btn-watch-video d-flex align-items-center"><i
                             class="bi bi-play-circle"></i><span>Watch Video</span></a>  --}}
-                </div>
             </div>
-            <div class="col-lg-6 order-lg-2 order-1">
-                <img src="{{ URL::asset('front/img/hero-img.svg') }}" class="img-fluid" alt=""
-                    data-aos="zoom-out" data-aos-delay="100">
-            </div>
+        </div>
+        <div class="col-lg-6 order-lg-2 order-1">
+            <img src="{{ URL::asset('front/img/hero-img.svg') }}" class="img-fluid" alt="" data-aos="zoom-out"
+                data-aos-delay="100">
         </div>
     </div>
+    </div>
 
-    <div class="icon-boxes position-relative">
-        <div class="position-relative container">
-            <div class="row gy-4 mt-5">
+    <div class="icon-boxes position-relative"></div>
+    <div class="position-relative container">
+        <div class="row gy-4 mt-5">
 
-                <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="icon-box">
-                        <div class="icon"><i class="bi bi-easel"></i></div>
-                        <h4 class="title"><a href="" class="stretched-link">Lorem Ipsum</a></h4>
-                    </div>
+            <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="icon-box">
+                    <div class="icon"><i class="bi bi-easel"></i></div>
+                    <h4 class="title"><a href="" class="stretched-link">Lorem Ipsum</a></h4>
                 </div>
-                <!--End Icon Box -->
-
-                <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
-                    <div class="icon-box">
-                        <div class="icon"><i class="bi bi-gem"></i></div>
-                        <h4 class="title"><a href="" class="stretched-link">Sed ut perspiciatis</a></h4>
-                    </div>
-                </div>
-                <!--End Icon Box -->
-
-                <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
-                    <div class="icon-box">
-                        <div class="icon"><i class="bi bi-geo-alt"></i></div>
-                        <h4 class="title"><a href="" class="stretched-link">Magni Dolores</a></h4>
-                    </div>
-                </div>
-                <!--End Icon Box -->
-
-                <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="500">
-                    <div class="icon-box">
-                        <div class="icon"><i class="bi bi-command"></i></div>
-                        <h4 class="title"><a href="" class="stretched-link">Nemo Enim</a></h4>
-                    </div>
-                </div>
-                <!--End Icon Box -->
-
             </div>
+            <!--End Icon Box -->
+
+            <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                <div class="icon-box">
+                    <div class="icon"><i class="bi bi-gem"></i></div>
+                    <h4 class="title"><a href="" class="stretched-link">Sed ut perspiciatis</a></h4>
+                </div>
+            </div>
+            <!--End Icon Box -->
+
+            <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                <div class="icon-box">
+                    <div class="icon"><i class="bi bi-geo-alt"></i></div>
+                    <h4 class="title"><a href="" class="stretched-link">Magni Dolores</a></h4>
+                </div>
+            </div>
+            <!--End Icon Box -->
+
+            <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="500">
+                <div class="icon-box">
+                    <div class="icon"><i class="bi bi-command"></i></div>
+                    <h4 class="title"><a href="" class="stretched-link">Nemo Enim</a></h4>
+                </div>
+            </div>
+            <!--End Icon Box -->
+
         </div>
+    </div>
     </div>
 
     </div>
